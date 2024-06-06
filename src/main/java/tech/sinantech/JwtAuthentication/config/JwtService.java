9f80cd6490920e8492510536950ca15f93d8c8cb
Sinan <sinancemozer@gmail.com>
2024-06-06 17:58:45 +0300

    feat:jwt service update

--- src/main/java/tech/sinantech/JwtAuthentication/config/JwtService.java
+++ src/main/java/tech/sinantech/JwtAuthentication/config/JwtService.java
@@ -1,11 +1,24 @@
 package tech.sinantech.JwtAuthentication.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "7FBD51C84A488877C1144D40FCBA547AADD2697B4EE6F8EFF448AFE70CFC1F2C";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
